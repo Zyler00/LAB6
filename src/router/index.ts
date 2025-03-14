a182f36b63df73787046975f380cf6cf774987f6
Zyler00 <chompoonoot_pimolrat@cmu.ac.th>
2025-03-15 00:25:53 +0700

    LAB6

---
+++ src/router/index.ts
@@ -0,0 +1,16 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import EventListView from '../views/EventListView.vue'
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: '/',
+      name: 'event-list-view',
+      component: EventListView,
+    },
+
+  ],
+})
+
+export default router
